60ac14b1d453f74ade40ac21879cceb00d5a2384
Benjamin Zhong <benjaminzhong1@gmail.com>
2026-06-07 14:36:19 -0400

    Feat: Add Signature -> Instruction Module
    - barebones structs
    - namespaces
    - creation tests

---
+++ include/instruction.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+namespace Instruction
+{
+    using universal_id = unsigned long long;
+    
+    enum Types;
+    struct Base;
+    struct ADD;
+}
+
+enum Instruction::Types
+{
+
+};
+
+struct Instruction::Base
+{
+    
+};
+
+struct Instruction::ADD : public Base
+{
+
+};

f3e8b084bdeef2bfbff966361998d3ec4ba27cbc
Benjamin Zhong <benjaminzhong1@gmail.com>
2026-06-12 09:24:33 -0400

    Feat: Injector Implementation
    - complete run
    - uncomplete stop method

--- include/injector.h
+++ include/injector.h
@@ -1,12 +1,21 @@
 #pragma once
 #include "ringBuffer.h"
+#include "fileReader.h"
+#include "instruction.h"
+#include "instructionBuilder.h"
+#include "memory"
 
 class Injector
 {
+protected:
+    FileReaderBase* m_fileReader { nullptr };
+    RingBuffer<std::shared_ptr<Instruction::Base>, 1024>* m_ringBuffer { nullptr };
+    InstructionBuilder* m_instructBuilder { nullptr };
+
 public:
-    Injector();
-    void run(/*File input_file, HighPerformanceRingBuffer buffer*/);
-    void stop();
+    Injector(FileReaderBase* fileReader, RingBuffer<std::shared_ptr<Instruction::Base>, 1024>* buffer);
+    virtual void run();
+    virtual void stop();
 };
 
 
--- src/injector.cpp
+++ src/injector.cpp
@@ -1,16 +1,24 @@
 #include "injector.h"
 
-Injector::Injector()
+Injector::Injector(FileReaderBase* fileReader, RingBuffer<std::shared_ptr<Instruction::Base>, 1024>* buffer)
 {
-
+    m_instructBuilder = new InstructionBuilder(fileReader);
 }
 
-void Injector::run(/*File input_file, HighPerformanceRingBuffer buffer*/)
+void Injector::run()
 {
-
+    while (true)
+    {
+        auto resInstruction = m_instructBuilder->get_instruction();
+        
+        while(!m_ringBuffer->push(resInstruction))
+        {
+            std::this_thread::yield();
+        }
+    }
 }
 
 void Injector::stop()
 {
-
+    // TODO: on your as exercise
 }

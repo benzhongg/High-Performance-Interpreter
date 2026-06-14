c3fecde489bd0a738807b8e6f151013daa2ca0b9
Benjamin Zhong <benjaminzhong1@gmail.com>
2026-06-14 14:00:39 -0400

    Refactor: cleaned up interpreter source file
    - code and comment cleanup
    - added stop method

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -7,12 +7,8 @@ Interpreter::Interpreter(RingBuffer<std::shared_ptr<Instruction::Base>,1024>* ri
 
 void Interpreter::run()
 {
-    // when you develop a run function in a thread you'll usually need a super loop
-
     while (true)
     {
-        //grabs from ringbuffer
-        //the instance of the shared ptr is a non ptr / regular variable
         std::shared_ptr<Instruction::Base> instruction { nullptr };
 
         while(!m_ringBuffer->pop(instruction))
@@ -20,19 +16,20 @@ void Interpreter::run()
             std::this_thread::yield();
         }
         
-        //executes instruction 
         switch (instruction->instructType)
         {
             case Instruction::InstructionType::ADD :
             {
                 auto addInstruction = static_cast<Instruction::Add*>(instruction.get());
                 
-                // std::uint32_t instructionResult { addInstruction->param1 + addInstruction->param2 } ;
-                // m_resultStack.push(instructionResult);
-            
-                //pushes to stack 
                 m_resultStack.push(addInstruction->param1 + addInstruction->param2);
+                std::cout << "pushing to stack succeeded\n";
             }
         }
     }
+}
+
+void Interpreter::stop()
+{
+
 }

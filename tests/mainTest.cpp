0b2c947334fef20518e811ac3fd9f237e6b3ae23
Benjamin Zhong <benjaminzhong1@gmail.com>
2026-06-15 08:23:11 -0400

    Feat: new Tests for main exec and instruction Builder

--- tests/mainTest.cpp
+++ tests/mainTest.cpp
@@ -3,20 +3,17 @@
 #include "injector.h"
 #include "interpreter.h"
 #include "fileReader.h"
+#include "stream_utils.h"
+#include <thread>
 
-TEST(InstructionBuilderCreationTest, StructsSuccessfullyCreated)
+TEST(mainTest, ModulesSuccessfullyCreated)
 {
-    char buffer[] = {0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00};
-    FileReaderBase* testFileReader =  new BufferFileReader(buffer, 12);
-    
+    FileReaderBase* testFileReader = new BufferFileReader(utils::makeFileStream("testData.txt"));
     InstructionBuilder testInstructionBuilder(testFileReader);
     RingBuffer<std::shared_ptr<Instruction::Base>, 1024>* testBuffer = new RingBuffer<std::shared_ptr<Instruction::Base>, 1024>();
     
     Injector testInjector(testFileReader, testBuffer);
-    testInjector.run(std::stop_thread stopThread);
+    Interpreter testInterpreter(testBuffer);
 
-    // Interpreter testInterpreter(testBuffer);
-    // testInterpreter.run();
-
-
+    std::thread threadA(&Injector::run, &testInjector);
 }
